35cba6d4f0652e65a85cd85164ba168cb8702c54
pawanprjl <pawanparajuli8@gmail.com>
2019-01-16 19:20:18 +0545

    menu items on action bar added

--- app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
+++ app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
@@ -2,6 +2,8 @@ package com.sanzaalgroup.btchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.bluetooth_chat, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }

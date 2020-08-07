d0ce86f7e64bdea5939e00223d3c7376bd3b792a
pawanprjl <pawanparajuli8@gmail.com>
2020-08-07 21:03:50 +0545

    updated device list layouts

--- app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
+++ app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
@@ -1,11 +1,16 @@
 package com.sanzaalgroup.btchat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    // intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.bluetooth_chat, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.secure_connect_scan: {
+                // open device list activity
+                Intent serverIntent = new Intent(this, DeviceListActivity.class);
+                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+                return true;
+            }
+        }
+        return false;
     }
 }

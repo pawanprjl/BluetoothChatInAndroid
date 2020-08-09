4c21c6c81bef922fcf113c7cbd2ff687bdc11967
pawanprjl <pawanparajuli8@gmail.com>
2020-08-10 00:24:42 +0545

    tutorial 5 updated

--- app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
+++ app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
@@ -1,21 +1,62 @@
 package com.sanzaalgroup.btchat;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // intent request codes
     private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+    // define bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get local adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // if adapter is null, bluetooth is not supported
+        if (mBluetoothAdapter == null){
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // if BT is not on, then request to turn it on
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case REQUEST_ENABLE_BT:
+                if(resultCode == Activity.RESULT_OK){
+
+                }else {
+                    Toast.makeText(this, "BT not enabled", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
     }
 
     @Override

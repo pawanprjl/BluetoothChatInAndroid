e764823a5f7f143b3b8b6af6ea2e246c60cf4e84
pawanprjl <pawanparajuli8@gmail.com>
2020-08-19 14:51:18 +0545

    added on click listeners for bluetooth devices

--- app/src/main/java/com/sanzaalgroup/btchat/DeviceListActivity.java
+++ app/src/main/java/com/sanzaalgroup/btchat/DeviceListActivity.java
@@ -1,12 +1,21 @@
 package com.sanzaalgroup.btchat;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Set;
 
@@ -14,17 +23,52 @@ public class DeviceListActivity extends AppCompatActivity {
 
     private BluetoothAdapter mBtAdapter;
 
+    // return intent extra
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+    // initialize new device adapter
+    ArrayAdapter<String> newDeviceArrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
 
+        // set result canceled when the user backs out
+        setResult(Activity.RESULT_CANCELED);
+
+
         // initialize adapters
         ArrayAdapter<String> pairedDeviceArrayAdapter = new ArrayAdapter<>(this, R.layout.device_name);
+
+        newDeviceArrayAdapter = new ArrayAdapter<>(this, R.layout.device_name);
+
 
         // initialize our listviews
         ListView pairedDevicesListView = findViewById(R.id.paired_devices);
         pairedDevicesListView.setAdapter(pairedDeviceArrayAdapter);
+        pairedDevicesListView.setOnItemClickListener(mDeviceClickListener);
+
+        ListView newDeviceListView = findViewById(R.id.new_devices);
+        newDeviceListView.setAdapter(newDeviceArrayAdapter);
+        newDeviceListView.setOnItemClickListener(mDeviceClickListener);
+
+        Button scanButton = findViewById(R.id.button_scan);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doDiscovery();
+                view.setVisibility(View.GONE);
+            }
+        });
+
+
+        // register receivers for broadcast
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);
+
 
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -42,4 +86,71 @@ public class DeviceListActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(mBtAdapter != null){
+            mBtAdapter.cancelDiscovery();
+        }
+
+        this.unregisterReceiver(mReceiver);
+    }
+
+    private void doDiscovery() {
+        setTitle("Scanning...");
+
+        findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
+
+        if(mBtAdapter.isDiscovering()){
+            mBtAdapter.cancelDiscovery();
+        }
+
+        if(!mBtAdapter.startDiscovery()){
+            Toast.makeText(this, "error during start discovery", Toast.LENGTH_SHORT).show();
+        };
+    }
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if(device.getBondState() != BluetoothDevice.BOND_BONDED){
+                    newDeviceArrayAdapter.add(device.getName()+"\n"+device.getAddress());
+                }
+            }else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                setTitle("Select Device");
+
+                if(newDeviceArrayAdapter.getCount() == 0){
+                    newDeviceArrayAdapter.add("no device found");
+                }
+            }
+        }
+    };
+
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            // cancel discovery because it's costly and we are about to connect
+            mBtAdapter.cancelDiscovery();
+
+            // get the device mac address
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+            // create the result intent and include the mac address
+            Intent intent = new Intent();
+            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+
+            // set the result
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+        }
+    };
+
 }
--- app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
+++ app/src/main/java/com/sanzaalgroup/btchat/MainActivity.java
@@ -1,8 +1,14 @@
 package com.sanzaalgroup.btchat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     // define bluetooth adapter
     private BluetoothAdapter mBluetoothAdapter = null;
 
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
         // get local adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         // if adapter is null, bluetooth is not supported
         if (mBluetoothAdapter == null){
@@ -43,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         if(!mBluetoothAdapter.isEnabled()){
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }else{
+            doLocationCheck();
         }
     }
 
@@ -51,13 +63,54 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode){
             case REQUEST_ENABLE_BT:
                 if(resultCode == Activity.RESULT_OK){
-
+                    doLocationCheck();
                 }else {
                     Toast.makeText(this, "BT not enabled", Toast.LENGTH_SHORT).show();
                     finish();
                 }
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                // when device list activity returns with a device to connect
+                if(resultCode == Activity.RESULT_OK){
+                    connectDevice(data, true);
+                }
         }
     }
+
+    // establish connection to other device
+    private void connectDevice(Intent data, boolean secure){
+        // get the device mac address
+        String address = data.getExtras().getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+
+        // get the bluetooth device object
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+
+        Toast.makeText(this, device.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void doLocationCheck(){
+        //check for location access
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Enable Location Access !");
+            builder.setMessage("This application wants to turn on location service. Are you sure to continue ?");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+
+            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MainActivity.this, "Some features may not work properly!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
